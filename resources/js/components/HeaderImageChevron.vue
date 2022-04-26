83697d39e7ccff4ff170371b00ce23642ca77bb6
DanEdney <dan@seadragon.digital>
2022-04-26 10:17:37 +0100

    Added missing component Services Header

---
+++ resources/js/components/HeaderImageChevron.vue
@@ -0,0 +1,29 @@
+<template>
+
+  <div id="HeaderImage" class="header-image chevron" :style="'background-image:url(\'/storage/images/headers/'+image+'\');'">
+
+    <div class="title-text p-3">
+      <div class="container">
+
+            <h1>{{ title }}</h1>
+        <p v-if="subtitle">{{ subtitle }}</p>
+        <router-link v-if="button" class="btn btn-primary btn-lg" to="/book">Book Online</router-link>
+
+      </div>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "HeaderImageChevron",
+  props: {
+    image: String,
+    title: String,
+    subtitle: String,
+    button:Boolean
+  }
+}
+</script>
