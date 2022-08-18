7fc3270209c8c826b1f658a09580a2c906a949ad
DanEdney <dan@seadragon.digital>
2022-08-18 13:11:45 +0100

    removed invalid case

--- resources/js/pages/auth/register.vue
+++ resources/js/pages/auth/register.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class=" flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+  <div class=" flex flex-col sm:justify-center items-center pt-6 sm:pt-0 ">
 
     <div class="w-full sm:max-w-md mt-6 mb-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
       <form @submit.prevent="register">
